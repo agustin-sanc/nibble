eb92b403bd68a093202c9d7e8000d948aca2df65
agustin-sanc <agustin_sz09@outlook.com>
2023-12-02 17:47:16 -0300

    feat: setup practices router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { postRouter } from "@/server/api/routers/post";
+import { practicesRouter } from "@/server/api/routers/practices";
 import { createTRPCRouter } from "@/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "@/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  practices: practicesRouter,
 });
 
 // export type definition of API
