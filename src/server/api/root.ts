c77fe9338dae5a433d8c4b347fec172a97a92476
agustin-sanc <agustin_sz09@outlook.com>
2023-12-03 01:15:11 -0300

    refactor: reorder imports

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,6 @@
+import { createTRPCRouter } from "@/server/api/trpc";
 import { practicesRouter } from "@/server/api/routers/practices";
 import { theoriesRouter } from "@/server/api/routers/theories";
-import { createTRPCRouter } from "@/server/api/trpc";
 import { exercisesRouter } from "@/server/api/routers/exercises";
 
 /**
