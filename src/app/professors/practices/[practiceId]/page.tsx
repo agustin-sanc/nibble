ee26321723ef23b0832ec537e664b30b6ba8369d
agustin-sanc <agustin_sz09@outlook.com>
2023-12-10 05:18:17 -0300

    add page for practice detail

---
+++ src/app/professors/practices/[practiceId]/page.tsx
@@ -0,0 +1,21 @@
+import { prisma } from "@/prisma";
+import InnerLayout from "@/app/_general/components/inner-layout";
+import { Header2, Header3 } from "@/app/_general/components/typography";
+
+export default async function Practice({
+  params: { practiceId },
+}: {
+  params: { practiceId: string };
+}) {
+  const practice = await prisma.practice.findUnique({
+    where: { id: Number(practiceId) },
+  });
+
+  return (
+    <InnerLayout>
+      <Header2>{practice?.name}</Header2>
+      <p>{practice?.description}</p>
+      <Header3>Ejercicios</Header3>
+    </InnerLayout>
+  );
+}
