3c5b932c2bb7b24893a0a77875ddf293a12e9217
agustin-sanc <agustin_sz09@outlook.com>
2023-12-10 05:18:30 -0300

    redirect to practice detail page

--- src/app/_general/components/practices/practice-card.tsx
+++ src/app/_general/components/practices/practice-card.tsx
@@ -1,25 +1,41 @@
+"use client";
+
 import { Binary, Laptop } from "lucide-react";
 import { Button } from "@/app/_general/components/button";
 import { Badge } from "@/app/_general/components/badge";
 import type { Practice, Exercise } from "@prisma/client";
+import Link from "next/link";
+import { useUser } from "@clerk/nextjs";
 
-type CompletePractice = Practice & { exercises: Exercise[] };
+type PracticeCardProps = {
+  practice: Practice & { exercises: Exercise[] };
+};
 
-export const PracticeCard = ({ practice }: { practice: CompletePractice }) => (
-  <div className="rounded border p-4">
-    <div className="mb-4 flex items-center justify-between">
-      <div className="flex items-center">
-        <Binary />
-        <h2 className="ml-2 text-xl font-bold">{practice.name}</h2>
+export const PracticeCard = ({ practice }: PracticeCardProps) => {
+  const { user } = useUser();
+
+  return (
+    <div className="rounded border p-4">
+      <div className="mb-4 flex items-center justify-between">
+        <div className="flex items-center">
+          <Binary />
+          <h2 className="ml-2 text-xl font-bold">{practice.name}</h2>
+        </div>
+
+        <Badge variant="outline">{practice.exercises.length} ejercicios</Badge>
       </div>
 
-      <Badge variant="outline">{practice.exercises.length} ejercicios</Badge>
+      <p className="pb-5 text-sm">{practice.description}</p>
+
+      <Link
+        href={`/${
+          !user?.publicMetadata.isProfessor ? "students" : "professors"
+        }/practices/${practice.id}`}
+      >
+        <Button className="flex items-center gap-2" variant="outline">
+          <Laptop /> Ver trabajo práctico
+        </Button>
+      </Link>
     </div>
-
-    <p className="pb-5 text-sm">{practice.description}</p>
-
-    <Button className="flex items-center gap-2" variant="outline">
-      <Laptop /> Resolver
-    </Button>
-  </div>
-);
+  );
+};
