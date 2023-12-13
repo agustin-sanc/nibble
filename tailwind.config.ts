322ad18db7038cb96e107e49503c32c68116d20f
agustin-sanc <agustin_sz09@outlook.com>
2023-12-13 19:49:53 -0300

    add keyframes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,20 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
   plugins: [],
