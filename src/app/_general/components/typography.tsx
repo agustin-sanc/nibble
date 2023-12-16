a3c69f955fbb5bfabb09f971bb5294bfc1163fa1
agustin-sanc <agustin_sz09@outlook.com>
2023-12-16 17:12:14 -0300

    add code typography

--- src/app/_general/components/typography.tsx
+++ src/app/_general/components/typography.tsx
@@ -25,3 +25,9 @@ export const Header3 = ({ children }: { children: ReactNode }) => (
     {children}
   </h3>
 );
+
+export const Code = ({ children }: { children: ReactNode }) => (
+  <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+    {children}
+  </code>
+);
