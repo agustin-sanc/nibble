5c9ae8e53ee66bd92619fa37789f23caa14c4ea5
agustin-sanc <agustin_sz09@outlook.com>
2023-12-17 19:28:29 -0300

    add comment

--- src/app/students/theories/page.tsx
+++ src/app/students/theories/page.tsx
@@ -2,6 +2,13 @@ import { TheoriesGrid } from "@/app/_general/components/theories/theories-grid";
 import LayoutWithSidePanel from "@/app/_general/components/layout-with-side-panel";
 import { Header2 } from "@/app/_general/components/typography";
 
+// import SyntaxHighlighter from "react-syntax-highlighter";
+// import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+// const codeString = "int resultado = promedio / 30;";
+// <SyntaxHighlighter language="cpp" style={docco}>
+//   {codeString}
+// </SyntaxHighlighter>
+
 export default function Theories() {
   return (
     <LayoutWithSidePanel>
