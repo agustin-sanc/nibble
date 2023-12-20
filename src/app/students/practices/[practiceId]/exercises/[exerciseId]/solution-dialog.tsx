fe9dda3b08dd7a1c5fd3754df1cc8412f2d8e342
agustin-sanc <agustin_sz09@outlook.com>
2023-12-20 16:39:10 -0300

    add language selector to solution dialog

--- src/app/students/practices/[practiceId]/exercises/[exerciseId]/solution-dialog.tsx
+++ src/app/students/practices/[practiceId]/exercises/[exerciseId]/solution-dialog.tsx
@@ -12,11 +12,18 @@ import { Button } from "@/app/_general/components/button";
 import { useState } from "react";
 import Editor from "@monaco-editor/react";
 import { useTheme } from "next-themes";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/app/_general/components/select";
 
 export const SolutionDialog = () => {
   const [code, setCode] = useState("");
   const { theme } = useTheme();
-  const [language, setLanguage] = useState<"C++" | "Python">("C++");
+  const [language, setLanguage] = useState<"cpp" | "python">("cpp");
 
   const submitSolution = () => {
     console.log("Send solution to evaluator server");
@@ -33,9 +40,27 @@ export const SolutionDialog = () => {
           <DialogTitle>Ingresa el código de tu solución aquí</DialogTitle>
         </DialogHeader>
 
+        <Select
+          onValueChange={(value) =>
+            value === "cpp" || value === "python"
+              ? setLanguage(value)
+              : console.error("Invalid language")
+          }
+          defaultValue={language}
+        >
+          <SelectTrigger className="w-full">
+            <SelectValue placeholder="Selecciona el lenguaje de programación" />
+          </SelectTrigger>
+
+          <SelectContent>
+            <SelectItem value="cpp">C++</SelectItem>
+            <SelectItem value="python">Python</SelectItem>
+          </SelectContent>
+        </Select>
+
         <Editor
           height="400px"
-          language={language === "C++" ? "cpp" : "python"}
+          language={language}
           theme={theme === "light" ? "vs-light" : "vs-dark"}
           value={code}
           onChange={(value) => setCode(value ?? "")}
