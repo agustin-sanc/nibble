f1e6a7ccd01ca074f1ffe164276a6cb2694caa38
agustin-sanc <agustin_sz09@outlook.com>
2023-12-23 11:23:06 -0300

    add list of related theories

--- src/app/students/practices/[practiceId]/page.tsx
+++ src/app/students/practices/[practiceId]/page.tsx
@@ -1,5 +1,10 @@
 import { prisma } from "@/prisma";
-import { Header2, Header3 } from "@/app/_general/components/typography";
+import {
+  Header2,
+  Header3,
+  Header4,
+  UnorderedList,
+} from "@/app/_general/components/typography";
 import LayoutWithSidePanel from "@/app/_general/components/layout-with-side-panel";
 import { ExercisesGrid } from "@/app/_general/components/exercises/exercises-grid";
 
@@ -10,13 +15,20 @@ export default async function Practice({
 }) {
   const practice = await prisma.practice.findUnique({
     where: { id: Number(practiceId) },
-    include: { exercises: true },
+    include: { exercises: true, theories: true },
   });
 
   return (
     <LayoutWithSidePanel>
       <Header2>{practice?.name}</Header2>
       <p>{practice?.description}</p>
+
+      <UnorderedList title="Unidades teóricas relacionadas">
+        {practice?.theories.map((theory) => (
+          <li key={theory.id}>{theory.name}</li>
+        ))}
+      </UnorderedList>
+
       <Header3>Ejercicios</Header3>
       <ExercisesGrid exercises={practice?.exercises ?? []} />
     </LayoutWithSidePanel>
