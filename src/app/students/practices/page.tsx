82b1a6f16f63811b2e3a29ab94ccaceba3117329
agustin-sanc <agustin_sz09@outlook.com>
2023-12-23 13:24:35 -0300

    fetch practices for practices page

--- src/app/students/practices/page.tsx
+++ src/app/students/practices/page.tsx
@@ -1,12 +1,17 @@
 import LayoutWithSidePanel from "@/app/_general/components/layout-with-side-panel";
 import { PracticesGrid } from "@/app/_general/components/practices/practices-grid";
 import { Header2 } from "@/app/_general/components/typography";
+import { prisma } from "@/prisma";
 
-export default function Practices() {
+export default async function Practices() {
+  const practices = await prisma.practice.findMany({
+    include: { exercises: true },
+  });
+
   return (
     <LayoutWithSidePanel>
       <Header2>Trabajos prácticos</Header2>
-      <PracticesGrid />
+      <PracticesGrid practices={practices} />
     </LayoutWithSidePanel>
   );
 }
