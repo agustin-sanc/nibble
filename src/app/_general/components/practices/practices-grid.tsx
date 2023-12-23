b8d9512050c293eacb3fb2e25023e5b2ab9d8a2e
agustin-sanc <agustin_sz09@outlook.com>
2023-12-23 15:41:36 -0300

    if refactor

--- src/app/_general/components/practices/practices-grid.tsx
+++ src/app/_general/components/practices/practices-grid.tsx
@@ -1,18 +1,21 @@
 import { PracticeCard } from "@/app/_general/components/practices/practice-card";
-import type { Exercise, Practice } from "@prisma/client";
+import { prisma } from "@/prisma";
 
-type PracticesGridProps = {
-  practices: (Practice & { exercises: Exercise[] })[];
+export const PracticesGrid = async () => {
+  const practices = await prisma.practice.findMany({
+    include: { exercises: true },
+  });
+
+  const existPractices = practices.length > 0;
+
+  return (
+    <div className="my-6 flex flex-wrap justify-between gap-5">
+      {!existPractices && <p>No hay trabajos prácticos para mostrar.</p>}
+
+      {existPractices &&
+        practices?.map((practice) => (
+          <PracticeCard key={practice.id} practice={practice} />
+        ))}
+    </div>
+  );
 };
-
-export const PracticesGrid = ({ practices }: PracticesGridProps) => (
-  <div className="my-6 flex flex-wrap justify-between gap-5">
-    {practices?.length === 0 ? (
-      <p>No hay trabajos prácticos para mostrar.</p>
-    ) : (
-      practices?.map((practice) => (
-        <PracticeCard key={practice.id} practice={practice} />
-      ))
-    )}
-  </div>
-);
