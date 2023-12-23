e7326e8e4431e395d863c3c7ef9e1274f13f0606
agustin-sanc <agustin_sz09@outlook.com>
2023-12-23 18:24:30 -0300

    padding fix

--- src/app/practices/[practiceId]/exercises/[exerciseId]/page.tsx
+++ src/app/practices/[practiceId]/exercises/[exerciseId]/page.tsx
@@ -43,7 +43,7 @@ export default async function Exercise({
     ));
 
   return (
-    <div className="flex flex-col gap-2 p-8">
+    <div className="flex flex-col gap-2 px-8 pt-8">
       <div>
         <Link
           href={`/practices/${practiceId}`}
