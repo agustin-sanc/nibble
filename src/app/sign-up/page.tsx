154f106086d27f878a0810dc6714d9d5d529cbb4
agustin-sanc <agustin_sz09@outlook.com>
2023-12-24 04:09:20 -0300

    add sign up page

---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,17 @@
+import { SignUp as ClerkSignUp } from "@clerk/nextjs";
+
+const SignUp = ({
+  params: { userType },
+}: {
+  params: { userType: "professor" | "student" };
+}) => {
+  const isProfessor = userType === "professor";
+
+  return (
+    <div className="flex w-full flex-col items-center justify-center gap-4 pt-8">
+      <ClerkSignUp unsafeMetadata={{ isProfessor }} />
+    </div>
+  );
+};
+
+export default SignUp;
