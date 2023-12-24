6690a216cb1ce958ef28aedbfdc45eb5ac5e54bc
agustin-sanc <agustin_sz09@outlook.com>
2023-12-24 01:50:46 -0300

    show related practices and add empty states

--- src/app/theories/[theoryId]/page.tsx
+++ src/app/theories/[theoryId]/page.tsx
@@ -1,6 +1,8 @@
 import { prisma } from "@/prisma";
 import { Header2, Header3 } from "@/app/_general/components/typography";
 import { LayoutWithSidePanel } from "@/app/_general/components/layout-with-side-panel";
+import { ContentGrid } from "@/app/_general/components/content-grid";
+import { ContentCard } from "@/app/_general/components/content-card";
 
 const Theory = async ({
   params: { theoryId },
@@ -12,13 +14,34 @@ const Theory = async ({
     include: { practices: { include: { exercises: true } } },
   });
 
+  let hasPractices = false;
+  if (theory && theory.practices) hasPractices = theory.practices.length > 0;
+
   return (
     <LayoutWithSidePanel>
-      <Header2>{theory?.name}</Header2>
-      <p>{theory?.description}</p>
+      {!theory && <p>La unidad teórica no existe.</p>}
 
-      <Header3>Trabajos prácticos sobre el tema</Header3>
-      {/*<ContentGrid />*/}
+      {theory && (
+        <>
+          <Header2>{theory.name}</Header2>
+          <p>{theory.description}</p>
+
+          <Header3>Trabajos prácticos sobre el tema</Header3>
+          {!hasPractices && <p>No hay prácticos sobre este tema aún.</p>}
+
+          {hasPractices && (
+            <ContentGrid>
+              {theory.practices.map((practice) => (
+                <ContentCard
+                  key={practice.id}
+                  type="practice"
+                  practice={practice}
+                />
+              ))}
+            </ContentGrid>
+          )}
+        </>
+      )}
     </LayoutWithSidePanel>
   );
 };
