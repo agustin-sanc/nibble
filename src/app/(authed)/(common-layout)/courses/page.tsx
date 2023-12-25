5f402c17766771efae27fd6a29a180fc6d5d2a67
agustin-sanc <agustin_sz09@outlook.com>
2023-12-25 02:50:32 -0300

    show professors button to create course

--- src/app/(authed)/(common-layout)/courses/page.tsx
+++ src/app/(authed)/(common-layout)/courses/page.tsx
@@ -2,14 +2,21 @@ import { ContentGrid } from "@/app/_common/components/content-grid";
 import { Header2 } from "@/app/_common/components/typography";
 import { prisma } from "@/app/_common/prisma";
 import { ContentCard } from "@/app/_common/components/content-card";
+import { Button } from "@/app/_common/components/button";
+import { currentUser } from "@clerk/nextjs";
 
 const Courses = async () => {
+  const user = await currentUser();
   const courses = await prisma.course.findMany({});
   const existCourses = courses.length > 0;
 
   return (
     <>
-      <Header2>Cursos</Header2>
+      <div className="flex justify-between">
+        <Header2>Cursos</Header2>
+
+        {user?.publicMetadata.isProfessor && <Button>Crear curso</Button>}
+      </div>
 
       <ContentGrid>
         {!existCourses && <p>No fuiste agregado a un curso aún.</p>}
