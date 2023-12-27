834c88f5bbc48335cbcb996cc281e3b9df6fbcab
agustin-sanc <agustin_sz09@outlook.com>
2023-12-26 21:56:12 -0300

    filter courses by session user

--- src/app/(main-layout)/courses/page.tsx
+++ src/app/(main-layout)/courses/page.tsx
@@ -3,11 +3,18 @@ import { Header2 } from "@/app/_cross/components/typography";
 import { prisma } from "@/app/_cross/prisma";
 import { ContentCard } from "@/app/_cross/components/content-card";
 import { currentUser } from "@clerk/nextjs";
-import { CreateCourseDialog } from "@/app/(main-layout)/courses/create-course-dialog";
+import { CreateCourseDialog } from "@/app/(main-layout)/courses/(create)/create-course-dialog";
 
 const Courses = async () => {
   const user = await currentUser();
-  const courses = await prisma.course.findMany({});
+  if (!user) return;
+
+  const courses = await prisma.course.findMany({
+    where: {
+      ownerId: user.id,
+    },
+  });
+
   const existCourses = courses.length > 0;
 
   return (
