ff61096537c17ceca5d3eda7cd77e1c64b07f6f0
agustin-sanc <agustin_sz09@outlook.com>
2023-12-28 22:18:44 -0300

    decrease min characters required

--- src/app/(main-layout)/courses/[courseId]/practices/(create)/create-practice-form-schema.ts
+++ src/app/(main-layout)/courses/[courseId]/practices/(create)/create-practice-form-schema.ts
@@ -5,12 +5,12 @@ export const createPracticeFormSchema = z.object({
     .string({
       required_error: "Tiene que tener un nombre",
     })
-    .min(5, "El nombre tiene que tener un mínimo de 5 caracteres")
+    .min(4, "El nombre tiene que tener un mínimo de 4 caracteres")
     .max(20, "El nombre tiene que tener un máximo de 20 caracteres"),
   description: z
     .string({
       required_error: "Tiene que tener una descripción",
     })
-    .min(5, "La descripción tiene que tener un mínimo de 5 caracteres")
+    .min(4, "La descripción tiene que tener un mínimo de 4 caracteres")
     .max(20, "La descripción tiene que tener un máximo de 20 caracteres"),
 });
