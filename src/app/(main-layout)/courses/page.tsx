b635dddd60e7446f57e694e2c7a76225ee772615
agustin-sanc <agustin_sz09@outlook.com>
2023-12-30 00:56:41 -0300

    improve courses page data handling

--- src/app/(main-layout)/courses/page.tsx
+++ src/app/(main-layout)/courses/page.tsx
@@ -2,33 +2,42 @@ import { ContentGrid } from "@/app/_cross/components/content-grid";
 import { Header2 } from "@/app/_cross/components/typography";
 import { prisma } from "@/app/_cross/prisma";
 import { ContentCard } from "@/app/_cross/components/content-card";
-import { currentUser } from "@clerk/nextjs";
 import { CreateCourseDialog } from "@/app/(main-layout)/courses/(create)/create-course-dialog";
+import { getCurrentUser } from "@/app/_cross/get-current-user";
 
 const Courses = async () => {
-  const user = await currentUser();
-
-  if (!user) throw new Error("User not found.");
+  const { id: currentUserId, isProfessor: currentUserIsProfessor } =
+    await getCurrentUser();
 
   const courses = await prisma.course.findMany({
     where: {
-      ownerId: user.id,
+      ...(currentUserIsProfessor
+        ? { ownerId: currentUserId }
+        : { studentIds: { has: currentUserId } }),
     },
   });
 
-  const existCourses = courses.length > 0;
+  const hasCourses = courses.length > 0;
+
+  console.log({ currentUserId, currentUserIsProfessor, courses });
 
   return (
     <>
       <div className="flex justify-between">
         <Header2>Cursos</Header2>
-        {user?.publicMetadata.isProfessor && <CreateCourseDialog />}
+        {currentUserIsProfessor && <CreateCourseDialog />}
       </div>
 
       <ContentGrid>
-        {!existCourses && <p>No fuiste agregado a un curso aún.</p>}
+        {!hasCourses && (
+          <p>
+            {currentUserIsProfessor
+              ? "No creaste un curso aún."
+              : "No fuiste agregado a un curso aún."}
+          </p>
+        )}
 
-        {existCourses &&
+        {hasCourses &&
           courses.map((course) => (
             <ContentCard key={course.id} type="course" course={course} />
           ))}
