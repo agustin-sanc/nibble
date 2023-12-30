3a3dded040e90c4d5a2466b197cac4befd7bbd12
agustin-sanc <agustin_sz09@outlook.com>
2023-12-30 01:52:23 -0300

    grid refactor

--- src/app/(main-layout)/courses/[courseId]/page.tsx
+++ src/app/(main-layout)/courses/[courseId]/page.tsx
@@ -2,18 +2,16 @@ import { prisma } from "@/app/_cross/prisma";
 import { Header2, Header3 } from "@/app/_cross/components/typography";
 import { ContentGrid } from "@/app/_cross/components/content-grid";
 import { ContentCard } from "@/app/_cross/components/content-card";
-import { currentUser } from "@clerk/nextjs";
 import { CreatePracticeDialog } from "@/app/(main-layout)/courses/[courseId]/practices/(create)/create-practice-dialog";
 import { CreateTheoryDialog } from "@/app/(main-layout)/courses/[courseId]/theories/(create)/create-theory-dialog";
+import { getCurrentUser } from "@/app/_cross/utils/get-current-user";
 
 const Course = async ({
   params: { courseId },
 }: {
   params: { courseId: string };
 }) => {
-  const user = await currentUser();
-
-  if (!user) throw new Error("User not found.");
+  const { isProfessor: currentUserIsProfessor } = await getCurrentUser();
 
   const course = await prisma.course.findUnique({
     where: { id: Number(courseId) },
@@ -39,18 +37,14 @@ const Course = async ({
   if (!course) return <p>El curso no existe</p>;
 
   const hasPractices = course.practices?.length > 0;
-  const hasTheories = course.theories?.length > 0;
 
-  return (
+  const PracticesSection = () => (
     <>
-      <Header2>{course.name}</Header2>
-      <p>{course.description}</p>
-
       <div className="flex flex-row items-center justify-between">
         <Header3>Trabajos prácticos</Header3>
 
-        {user?.publicMetadata.isProfessor && (
-          <CreatePracticeDialog courseId={Number(courseId)} />
+        {currentUserIsProfessor && (
+          <CreatePracticeDialog courseId={course.id} />
         )}
       </div>
 
@@ -67,13 +61,16 @@ const Course = async ({
           ))}
         </ContentGrid>
       )}
+    </>
+  );
 
+  const hasTheories = course.theories?.length > 0;
+
+  const TheoriesSection = () => (
+    <>
       <div className="flex flex-row items-center justify-between">
         <Header3>Unidades teóricas</Header3>
-
-        {user?.publicMetadata.isProfessor && (
-          <CreateTheoryDialog courseId={Number(courseId)} />
-        )}
+        {currentUserIsProfessor && <CreateTheoryDialog courseId={course.id} />}
       </div>
 
       {!hasTheories && <p>No hay unidades teóricas aún.</p>}
@@ -85,6 +82,16 @@ const Course = async ({
           ))}
         </ContentGrid>
       )}
+    </>
+  );
+
+  return (
+    <>
+      <Header2>{course.name}</Header2>
+      <p>{course.description}</p>
+
+      <PracticesSection />
+      <TheoriesSection />
     </>
   );
 };
