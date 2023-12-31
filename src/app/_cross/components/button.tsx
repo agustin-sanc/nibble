3c1cd017daf2c6c3312d7b16f2c9aa81077a6901
agustin-sanc <agustin_sz09@outlook.com>
2023-12-31 02:08:46 -0300

    improve validations

--- src/app/_cross/components/button.tsx
+++ src/app/_cross/components/button.tsx
@@ -41,17 +41,26 @@ export interface ButtonProps
   loading?: boolean;
 }
 
-// TODO: Add loading spinner
-
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    {
+      className,
+      variant,
+      size,
+      asChild = false,
+      loading = false,
+      disabled = false,
+      ...props
+    },
+    ref,
+  ) => {
     const Comp = asChild ? Slot : "button";
 
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
-        disabled={props.disabled ?? props.loading}
+        disabled={disabled || loading}
         {...props}
       />
     );
