48646e02c375cc61f1f933919c9c403df7e41a36
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-05-26 21:48:40 -0300

    fix: theories fetch query and empty state

--- src/app/_cross/components/latest-theories.tsx
+++ src/app/_cross/components/latest-theories.tsx
@@ -5,15 +5,26 @@ import { Header2 } from "@/app/_cross/components/typography";
 import { ContentGrid } from "@/app/_cross/components/content-grid";
 import { ContentCard } from "@/app/_cross/components/content-card";
 import Link from "next/link";
+import { getCurrentUser } from "@/app/_cross/auth/get-current-user";
+import { EmptyState } from "@/app/_cross/components/empty-state";
 
 export const LatestTheories = async () => {
+  const user = await getCurrentUser();
+
+  if (!user) throw new Error("User not found");
+
   const theories = await prisma.theory.findMany({
+    where: {
+      ...(user.isProfessor
+        ? { course: { ownerId: user.id } }
+        : { course: { studentIds: { has: user.id } } }),
+    },
     orderBy: { createdAt: "desc" },
     include: { practices: true },
     take: 4,
   });
 
-  const existTheories = theories.length > 0;
+  const hasTheories = theories.length > 0;
 
   return (
     <div className="mt-7">
@@ -23,17 +34,21 @@ export const LatestTheories = async () => {
           <Header2 className="ml-2 mt-2">Últimas unidades teóricas</Header2>
         </div>
 
-        <Button className="flex items-center gap-2" variant="outline" asChild>
-          <Link href="/theories" className="flex items-center gap-2">
-            Ver todas <ArrowRight />
-          </Link>
-        </Button>
+        {hasTheories && (
+          <Button className="flex items-center gap-2" variant="outline" asChild>
+            <Link href="/theories" className="flex items-center gap-2">
+              Ver todas <ArrowRight />
+            </Link>
+          </Button>
+        )}
       </div>
 
       <ContentGrid>
-        {!existTheories && <p>No hay unidades teóricas para mostrar.</p>}
+        {!hasTheories && (
+          <EmptyState title="No hay unidades teóricas para mostrar" />
+        )}
 
-        {existTheories &&
+        {hasTheories &&
           theories?.map((theory) => (
             <ContentCard key={theory.id} type="theory" theory={theory} />
           ))}
