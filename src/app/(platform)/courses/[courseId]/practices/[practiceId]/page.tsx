8e8b9c254728c895e3a863010579843f79ff9ef3
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-06-10 00:00:05 -0300

    refactor: change not found handling

--- src/app/(platform)/courses/[courseId]/practices/[practiceId]/page.tsx
+++ src/app/(platform)/courses/[courseId]/practices/[practiceId]/page.tsx
@@ -2,6 +2,7 @@ import { database } from "@/app/_cross/database";
 import { Header2, Header3 } from "@/app/_cross/components/typography";
 import { ContentGrid } from "@/app/_cross/components/content-grid";
 import { ContentCard } from "@/app/_cross/components/content-card";
+import { notFound } from "next/navigation";
 
 const Practice = async ({
   params: { practiceId },
@@ -13,44 +14,40 @@ const Practice = async ({
     include: { exercises: { include: { practice: true } }, theories: true },
   });
 
+  if (!practice) notFound();
+
   const hasExercises = practice && practice?.exercises?.length > 0;
   const hasRelatedTheories = practice && practice?.theories?.length > 0;
 
   return (
     <>
-      {!practice && <p>El trabajo práctico no existe.</p>}
+      <Header2>{practice.name}</Header2>
+      <p>{practice.description}</p>
 
-      {practice && (
-        <>
-          <Header2>{practice.name}</Header2>
-          <p>{practice.description}</p>
+      <Header3>Ejercicios</Header3>
+      {!hasExercises && <p>No hay ejercicios aún.</p>}
 
-          <Header3>Ejercicios</Header3>
-          {!hasExercises && <p>No hay ejercicios aún.</p>}
+      {hasExercises && (
+        <ContentGrid>
+          {practice.exercises.map((exercise) => (
+            <ContentCard
+              key={exercise.id}
+              type="exercise"
+              exercise={exercise}
+            />
+          ))}
+        </ContentGrid>
+      )}
 
-          {hasExercises && (
-            <ContentGrid>
-              {practice.exercises.map((exercise) => (
-                <ContentCard
-                  key={exercise.id}
-                  type="exercise"
-                  exercise={exercise}
-                />
-              ))}
-            </ContentGrid>
-          )}
+      <Header3>Teoría relacionada</Header3>
+      {!hasRelatedTheories && <p>No hay teoría relacionada.</p>}
 
-          <Header3>Teoría relacionada</Header3>
-          {!hasRelatedTheories && <p>No hay teoría relacionada.</p>}
-
-          {hasRelatedTheories && (
-            <ContentGrid>
-              {practice.theories.map((theory) => (
-                <ContentCard key={theory.id} type="theory" theory={theory} />
-              ))}
-            </ContentGrid>
-          )}
-        </>
+      {hasRelatedTheories && (
+        <ContentGrid>
+          {practice.theories.map((theory) => (
+            <ContentCard key={theory.id} type="theory" theory={theory} />
+          ))}
+        </ContentGrid>
       )}
     </>
   );
