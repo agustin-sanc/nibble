5671fe6c60e53d30a971d34ebbcc1c50bb0e3267
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-11-08 18:41:26 -0300

    chore: unmock

--- src/app/(platform)/courses/[courseId]/reports/page.tsx
+++ src/app/(platform)/courses/[courseId]/reports/page.tsx
@@ -22,121 +22,121 @@ async function CourseReportsPage({ params }: { params: { courseId: string } }) {
     },
   });
 
-  // const report = await fetch("http://localhost:8082/api/report", {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify({
-  //     practices: practices.map((practice) => ({
-  //       id: practice.id,
-  //       excercises: practice.exercises.map((exercise) => ({
-  //         id: exercise.id,
-  //         tags: exercise.tags.map((tag) => tag.name),
-  //         difficulty: exercise.difficulty as number,
-  //       })),
-  //     })),
-  //   }),
-  // }).then((res) => res.json()) as {
-  //   succeded_practices_ratio: number;
-  //   succeded_excercises_ratio: number;
-  //   practices: Record<string, {
-  //     succeded_attempts: number;
-  //     failed_attempts: number;
-  //     succeded_ratio: number;
-  //   }>;
-  //   tags: Record<string, {
-  //     succeded_attempts: number;
-  //     failed_attempts: number;
-  //     succeded_ratio: number;
-  //   }>;
-  //   difficulties: Record<string, {
-  //     succeded_attempts: number;
-  //     failed_attempts: number;
-  //     succeded_ratio: number;
-  //   }>;
-  // };
-
-
-  // const studentsEvaluation = await fetch("http://localhost:8082/api/evaluation", {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify({
-  //     studentIds: course.studentIds,
-  //     practices: practices.map((practice) => ({
-  //       id: practice.id,
-  //       excercises: practice.exercises.map((exercise) => ({
-  //         id: exercise.id,
-  //         tags: exercise.tags.map((tag) => tag.name),
-  //         difficulty: exercise.difficulty as number,
-  //       })),
-  //     })),
-  //   }),
-  // }).then((res) => res.json()) as {
-  //   general_excercise_difficulty: number;
-  //   course_resolve_capacity: number;
-  //   student_evaluations: Record<string, {
-  //     resolution_score: number;
-  //     resolution_capacity: number;
-  //   }>;
-  // };
-
-  const studentsEvaluation = {
-    general_excercise_difficulty: 1.5,
-    course_resolve_capacity: 0.75,
-    student_evaluations: {
-      "1": { resolution_score: 0.8, resolution_capacity: 0.9 },
-      "2": { resolution_score: 0.6, resolution_capacity: 0.7 },
+  const report = await fetch("http://localhost:8082/api/report", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
     },
+    body: JSON.stringify({
+      practices: practices.map((practice) => ({
+        id: practice.id,
+        excercises: practice.exercises.map((exercise) => ({
+          id: exercise.id,
+          tags: exercise.tags.map((tag) => tag.name),
+          difficulty: exercise.difficulty as number,
+        })),
+      })),
+    }),
+  }).then((res) => res.json()) as {
+    succeded_practices_ratio: number;
+    succeded_excercises_ratio: number;
+    practices: Record<string, {
+      succeded_attempts: number;
+      failed_attempts: number;
+      succeded_ratio: number;
+    }>;
+    tags: Record<string, {
+      succeded_attempts: number;
+      failed_attempts: number;
+      succeded_ratio: number;
+    }>;
+    difficulties: Record<string, {
+      succeded_attempts: number;
+      failed_attempts: number;
+      succeded_ratio: number;
+    }>;
   };
 
-  const report = {
-    succeded_practices_ratio: 0.75,
-    succeded_excercises_ratio: 0.68,
-    practices: {
-      "TP1": {
-        succeded_attempts: 25,
-        failed_attempts: 10,
-        succeded_ratio: 0.71
-      },
-      "TP2": {
-        succeded_attempts: 30,
-        failed_attempts: 5,
-        succeded_ratio: 0.86
-      },
-      "TP3": {
-        succeded_attempts: 30,
-        failed_attempts: 5,
-        succeded_ratio: 0.86
-      }
+
+  const studentsEvaluation = await fetch("http://localhost:8082/api/evaluation", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
     },
-    tags: {
-      "recursion": {
-        succeded_attempts: 45,
-        failed_attempts: 15,
-        succeded_ratio: 0.75
-      },
-      "loops": {
-        succeded_attempts: 60,
-        failed_attempts: 20,
-        succeded_ratio: 0.75
-      }
-    },
-    difficulties: {
-      "1": {
-        succeded_attempts: 40,
-        failed_attempts: 10,
-        succeded_ratio: 0.80
-      },
-      "2": {
-        succeded_attempts: 30,
-        failed_attempts: 15,
-        succeded_ratio: 0.67
-      }
-    }
+    body: JSON.stringify({
+      studentIds: course.studentIds,
+      practices: practices.map((practice) => ({
+        id: practice.id,
+        excercises: practice.exercises.map((exercise) => ({
+          id: exercise.id,
+          tags: exercise.tags.map((tag) => tag.name),
+          difficulty: exercise.difficulty as number,
+        })),
+      })),
+    }),
+  }).then((res) => res.json()) as {
+    general_excercise_difficulty: number;
+    course_resolve_capacity: number;
+    student_evaluations: Record<string, {
+      resolution_score: number;
+      resolution_capacity: number;
+    }>;
   };
+
+  // const studentsEvaluation = {
+  //   general_excercise_difficulty: 1.5,
+  //   course_resolve_capacity: 0.75,
+  //   student_evaluations: {
+  //     "1": { resolution_score: 0.8, resolution_capacity: 0.9 },
+  //     "2": { resolution_score: 0.6, resolution_capacity: 0.7 },
+  //   },
+  // };
+
+  // const report = {
+  //   succeded_practices_ratio: 0.75,
+  //   succeded_excercises_ratio: 0.68,
+  //   practices: {
+  //     "TP1": {
+  //       succeded_attempts: 25,
+  //       failed_attempts: 10,
+  //       succeded_ratio: 0.71
+  //     },
+  //     "TP2": {
+  //       succeded_attempts: 30,
+  //       failed_attempts: 5,
+  //       succeded_ratio: 0.86
+  //     },
+  //     "TP3": {
+  //       succeded_attempts: 30,
+  //       failed_attempts: 5,
+  //       succeded_ratio: 0.86
+  //     }
+  //   },
+  //   tags: {
+  //     "recursion": {
+  //       succeded_attempts: 45,
+  //       failed_attempts: 15,
+  //       succeded_ratio: 0.75
+  //     },
+  //     "loops": {
+  //       succeded_attempts: 60,
+  //       failed_attempts: 20,
+  //       succeded_ratio: 0.75
+  //     }
+  //   },
+  //   difficulties: {
+  //     "1": {
+  //       succeded_attempts: 40,
+  //       failed_attempts: 10,
+  //       succeded_ratio: 0.80
+  //     },
+  //     "2": {
+  //       succeded_attempts: 30,
+  //       failed_attempts: 15,
+  //       succeded_ratio: 0.67
+  //     }
+  //   }
+  // };
 
   console.dir({ report, studentsEvaluation }, { depth: null });
 
